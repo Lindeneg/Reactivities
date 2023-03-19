3235803391e658d40d99cbb4cf860006cebb0732
lindeneg <christian@lindeneg.org>
2023-03-20 00:55:40 +0100

    update

--- API/Program.cs
+++ API/Program.cs
@@ -3,8 +3,8 @@ using API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddCustomServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
